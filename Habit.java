f62c3f69b8f3965a22f218ec5d5cbfe32ce11b8b
codeRed-J <svjatoslav.matvejev@gmail.com>
2020-12-12 21:57:56 +0200

    Improv constructor and addDay()
    Resolved some bug problem in constructor and addDay() method.
    Add variables: startDate, lastDate, daysDo, totlaDays, file

--- Habit.java
+++ Habit.java
@@ -3,65 +3,95 @@
  import java.io.FileWriter;
  import java.io.IOException;
 
-// Create constructor
-// Add first date variable(constant)
-// Add variable that holds last date when habit is done
-// Total days spent for habit - variable that always increments
+// rename name
+// rename file name
 public class Habit{
 	
 	// Habit name
 	private String name;
 	// Data of habit creation
-	private LocalDate theDate;
+	private final LocalDate startDate;
+	// Date when done last time
+	private LocalDate lastDate;
 	// Days you do some habit in a row
-	private int success;
+	private int daysDo;
 	// Max days in a row
 	private int record;
+	// Total days done
+	private int totalDays;
+	// Create name of file where will be stored information about habit
+	private String file;
 	
-	
-	Habit(String name, boolean done) throws IOException {
+	// Create a habit object. 
+	// If parameter done is true, it means that you done it in a day of creation
+	// Otherwise you just create a habit	
+	public Habit(String name, boolean done) throws IOException {
 		this.name = name;
-		this.theDate = LocalDate.now();
+		this.startDate = LocalDate.now();
 		if(done){
-			this.success = 1;
+			// If habit done at the moment when it was created
+			this.lastDate = this.startDate;
+			this.daysDo = 1;
 			this.record = 1;
+			this.totalDays = 1;
 		}
 		else{
-			this.success = 0;
+			// If only habit was created, lastDate variable will initialize 
+			// to date which is two days before creation date.
+			// It is done to compare it in method addDay()
+			this.lastDate = LocalDate.now().minusDays(2);
+			this.daysDo = 0;
 			this.record = 0;
+			this.totalDays = 0;
 		} 
-	
+		this.file = name + ".txt";
+		
 				
-		FileWriter fr = new FileWriter("habits.txt", true);
+		FileWriter fr = new FileWriter(file);
+		// Create string with titles
+		String habitTitle = "Name\tCreated\tSast done\tRecord\tStreak";
 		// Create string with habit details
-		String habitDetails = name + " " + success + " " + record + " " + theDate;
+		String habitDetails = name + " " + startDate + " " + lastDate + " " + record + " " + daysDo;
+		// Write to txt file string titles
+		fr.write(habitTitle);
 		// Write to txt file string with details
 		fr.write(habitDetails);
 		fr.append("\n");
 		fr.close();
 	}	
 	
-	public void addDay(LocalDate date){
-		if(date.isAfter(theDate)){
-			success++;
-			theDate = date;
-			if(record < success){
-			record = success;
+	// Add date when you did a habit.
+	public void addDay(LocalDate date) throws IOException {
+		// Check if habit already done today
+		if(lastDate.equals(date)){
+			System.out.println("You already did it today!");
+		}
+		else {//lastDate.equals(date)){
+			totalDays++;
+			// Checks if you go day by day or start after some break
+			if((date.minusDays(1)).equals(lastDate)){
+				daysDo++;
+				lastDate = date;
+			}
+			else{
+				lastDate = date;
+				daysDo = 1;
+			}
+			// Compare streak to record
+			if(record < daysDo){
+				record = daysDo;
 			}
 		}
-		else{
-			theDate = date;
-			success = 1;
-		}
-		System.out.println("Data successfuly added!");
-		FileWriter fr = new FileWriter("habits.txt", true);
+		
+		FileWriter fr = new FileWriter(file, true);
 		// Create string with habit details
-		String habitDetails = name + " " + success + " " + record + " " + theDate;
+		String habitDetails = name + " " + startDate + " " + lastDate + " " + record + " " + daysDo + " " + totalDays;
 		// Write to txt file string with details
 		fr.write(habitDetails);
 		fr.append("\n");
 		fr.close();
 	}
+}
 	
 	//Map<A,List<B>> map = new HashMap<A,List<B>>();
 	
@@ -69,8 +99,8 @@ public class Habit{
 		//return name;
 	//}
 	
-	public void show(){
-		System.out.printf("You did %s %d days in a row", name, success);
+/*	public void show(){
+		System.out.printf("You did %s %d days in a row", name, daysDo);
 	}
 	
 	public static void habitDialogue(){
@@ -84,3 +114,4 @@ public class Habit{
 	}
 	
 }
+*/
--- HabitsTest.java
+++ HabitsTest.java
@@ -1,15 +1,21 @@
 import java.io.IOException;
+import java.time.LocalDate;
 
 public class HabitsTest{
 	public static void main(String[] args){
-		
+				
 		try{
-			Habbit habbit = new Habbit("programming", true);
+			Habit habit = new Habit("programming", true);
+			habit.addDay(LocalDate.now());
+			habit.addDay(LocalDate.of(2020, 12, 13));
+			habit.addDay(LocalDate.of(2020, 12, 14));
+			habit.addDay(LocalDate.of(2020, 12, 16));
+			habit.addDay(LocalDate.of(2020, 12, 17));
+			habit.addDay(LocalDate.of(2020, 12, 18));
+			habit.addDay(LocalDate.of(2020, 12, 19));
 		}
 		catch(IOException e){
 			System.out.println("Can't open file");
 		}
-		
-		habbit.addDay();
 	}
 }
